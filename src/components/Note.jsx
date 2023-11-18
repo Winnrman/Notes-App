9c1e3c74cbf003d67110625b9b0b319c582bd5e3
Andre Schoolman <andre@Andres-MacBook-Pro.local>
2023-11-18 00:28:14 -0500

    made UI look modern

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -6,13 +6,15 @@ const EditNote = (id) => {
 }
 const note = (props) => {
     return (
-        <div key={note._id} className = " bg-white rounded-md p-2 w-96 text-sm">
+        <div key={note._id} className = "w-auto sm:w-96 m-4 bg-white rounded-md p-2 text-sm hover:bg-white/90 hover:cursor-pointer hover:ring-2 hover:ring-slate-600 shadow-md">
             <h3 className = "font-semibold text-xl">{props.note.title}</h3>
             <p className = "text-sm font-light text-gray-500 pt-4">{props.note.content}</p>
-            <div className = "bg-slate-100 p-2 flex items-center justify-between">
-                <p className = "text-xs">{props.note.dateCreated}</p>
+            <div className = "bg-transparent p-2 flex items-center justify-between">
+                {/* <p className = "text-xs">{props.note.dateCreated}</p> */}
+                <span></span>
                 <div className = "flex flex-row gap-2 items-center">
-                <button onClick = {(e) => starNote(note._id)} className = "bg-transparent hover:bg-slate-200 text-white font-bold p-2 rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="fill-transparent text-blue-500 w-6 h-6">
+                
+                <button onClick = {(e) => starNote(note._id)} className = "bg-transparent hover:bg-slate-200 text-white font-bold p-2 rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="fill-transparent text-blue-500 w-6 h-6 hover:text-transparent hover:fill-yellow-400">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                 </svg>
                 </button>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -55,22 +55,26 @@ const Dashboard = () => {
     
 
     return (
-        <div>
-            <Header/>
+        <>
+        <Header/>
+        <div className = "bg-gradient-to-bl from-purple-900 to-blue-900 min-h-screen"> 
             {/* Render user data here */}
-            <div className = "pt-16 flex items-center justify-between m-4">
-            <h1 className = "text-2xl font-bold">Welcome, {userData.firstName}!</h1>
-            <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.location = "/notes/new"}>Create a note</button>
+            <div className = "pt-16 flex items-center justify-between mx-4">
+            <h1 className = "text-2xl font-bold text-gradient bg-gradient-to-r from-purple-300 to-blue-300 text-transparent bg-clip-text">Welcome, {userData.firstName}!</h1>
+            <button className = "flex items-center bg-slate-100/10 hover:bg-slate-100/20 text-white font-light py-2 px-3 rounded" onClick={() => window.location = "/notes/new"}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M12 5.25a.75.75 0 01.75.75v5.25H18a.75.75 0 010 1.5h-5.25V18a.75.75 0 01-1.5 0v-5.25H6a.75.75 0 010-1.5h5.25V6a.75.75 0 01.75-.75z" clipRule="evenodd" />
+</svg>
+New</button>
         </div>
 
             {userData.notes.length > 0 ? (
-                <div className = "w-full bg-slate-200 p-2 m-4 rounded-md">
-                    <h2 className = "text-xl font-semibold">Your Notes</h2>
-                    <ul className = "bg-slate-200 p-2 flex gap-2">
+                <div className = "w-full bg-transparent p-2 rounded-md">
+                    {/* <h2 className = "text-xl font-semibold text-white">Your Notes</h2> */}
+                    <div className = "bg-transparent py-2 flex flex-col sm:flex-row ">
                         {notes.map(note => (
                             <Note note = {note}></Note>
                         ))}
-                    </ul>
+                    </div>
                 </div>
             ) : (
                 <div>
@@ -78,8 +82,10 @@ const Dashboard = () => {
                     <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.location = "/notes/new"}>Create a note</button>    
                 </div>
             )}
-        <div className = "w-auto bg-slate-300 p-2 m-4 rounded-md">
-            <h1 className = "text-xl font-semibold">Starred Notes</h1>
+        <div className = "w-auto bg-slate-100/30 p-2 mb-32 mx-6 rounded-md">
+            <h1 className = "flex items-center gap-1 text-xl font-semibold text-white/80"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+</svg>Starred Notes</h1>
             {userData.starredNotes.length > 0 ? (
                 <ul>
                     {userData.starredNotes.map(note => (
@@ -88,20 +94,23 @@ const Dashboard = () => {
                 </ul>
             ) : (
                 <div>
-                    <h2 className = "text-xs text-center">No starred notes.</h2>
+                    <h2 className = "text-xs text-center text-white/60 py-4">No starred notes.</h2>
                 </div>
             )}
         </div>
 
-        <div className = "w-full bg-slate-200 p-2">
-            <h1 className = "text-xl font-semibold">Recent Notes</h1>
+        <div className = "w-auto rounded-md bg-slate-100/10 m-6 py-2">
+            <h1 className = "flex items-center text-xl font-semibold text-white/70 px-4 gap-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clipRule="evenodd" />
+</svg>
+Recent Notes</h1>
             {/* show the 5 most recent notes */}
             {userData.notes.length > 0 ? (
-                <ul className = "bg-slate-200 p-2 flex gap-2">
+                <div className = "rounded-md flex flex-col sm:flex-row">
                     {notes.slice(0,5).map(note => (
                         <Note note = {note}></Note>
                     ))} 
-                </ul>
+                </div>
             ) : (
                 <div>
                     <h2 className = "text-xs text-center">No recent notes.</h2>
@@ -109,8 +118,9 @@ const Dashboard = () => {
             )}
         </div>
 
-        <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>Logout</button>
+        <button className = "bg-transparent hover:bg-slate-100/20 text-white font-light py-2 px-3 rounded mx-6" onClick={() => logout()}>Logout</button>
         </div>
+        </>
     );
 };
 
