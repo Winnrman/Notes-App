d1129c581b0e9ec749c17db7f9b06cb2d25be1b0
Andre Schoolman <andre@Andres-MacBook-Pro.local>
2023-11-18 02:54:03 -0500

    added dropdown menu which displays tier and system settings link

---
+++ src/components/DropdownMenu.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const DropdownMenu = ({ userData }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
+  return (
+    <div ref={dropdownRef} className="relative inline-block text-left">
+      <div onClick={toggleDropdown} className="cursor-pointer">
+        {/* User SVG Icon Here */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 fill-blue-500">
+          <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
+        </svg>
+        {/* {userData && <p className="text-slate-100/60 text-md font-semibold">{userData.firstName}</p>} */}
+      </div>
+      {isOpen && (
+        <div className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {/* Dropdown items here */}
+          {userData ? (
+            <>
+          <p className="justify-between flex block px-4 py-2 text-sm text-purple-900 font-bold hover:bg-gray-100 hover:rounded-md">{userData.accountType}<span className = "font-light">Change</span></p>
+          <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:rounded-md">Account Settings</a>
+          </>
+            ) : (
+            <p className="justify-between flex block px-4 py-2 text-sm text-purple-900 font-bold hover:bg-gray-100 hover:rounded-md">Sign In</p>
+            )}
+          {/* ... other links ... */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import DropdownMenu from "./DropdownMenu";
 import { fetchUserData } from "./userService";
 import React, { useEffect, useState } from 'react';
 
@@ -37,16 +38,16 @@ useEffect(() => {
     return (
         <div style={{ background: `rgba(90, 11, 163, ${opacity})` }} className="fixed flex w-screen p-4 justify-between">
             <h1 onClick = {handleClick} className = "hover:cursor-pointer font-bold flex gap-2 items-center text-2xl text-slate-100/60"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 fill-slate-100/60">
-  <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
-</svg>
-Noteify</h1>
-            <div className = "flex flex-row items-center gap-1">
-            {userData && <p className = "text-slate-100/60 text-md font-semibold">{userData.firstName}</p>}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 fill-blue-500">
-            <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
+        <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
             </svg>
+            Noteify</h1>
+            <div className = "flex flex-row items-center gap-1">
+                {userData && <p className = "text-slate-100/60 text-md font-semibold">{userData.firstName}</p>}
+                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 fill-blue-500">
+                <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
+                </svg> */}
+                <DropdownMenu userData={userData} />
             </div>
-
         </div>
     )
 }
