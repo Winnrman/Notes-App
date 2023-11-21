61f2fbb2eceffff617206a1fb046bf2013e0e7ec
Andre Schoolman <andre@Andres-MacBook-Pro.local>
2023-11-20 21:28:27 -0500

    began working on search

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -153,6 +153,10 @@ const Dashboard = () => {
         }
     }
 
+    const handleSearch = async () => {
+        alert("Search function is currently under development")
+    }
+
 
     useEffect(() => {
         const fetchStarredNotes = async () => {
@@ -181,9 +185,9 @@ const Dashboard = () => {
         <Header/>
         {viewingUpgradeNotification && (
             <div className="fixed top-0 left-0 right-0 bottom-0 bg-black/50 flex items-center justify-center">
-                <div className="bg-white rounded-md p-4 gap-2 flex flex-col">
+                <div className="bg-white w-3/4 sm:w-fit rounded-md p-4 gap-2 flex flex-col">
                     <div className="flex justify-between items-center gap-2">
-                        <h1 className="text-xl font-bold">Upgrade to Premium</h1>
+                        <h1 className="text-xl font-bold">Notes Limit Reached</h1>
                         <button
                             className="w-8 h-8 hover:bg-slate-100 rounded-md items-center flex justify-center"
                             onClick={() => setViewingUpgradeNotification(false)}
@@ -204,7 +208,7 @@ const Dashboard = () => {
                         </button>
                     </div>
                     <p className="text-sm">
-                        You have reached the maximum number of notes for a free user. Upgrade to Premium to create unlimited notes.
+                        You have reached the maximum number of notes for a free user. Upgrade to Pro to create unlimited notes.
                     </p>
                     <div className="flex justify-between gap-2">
                         <button
@@ -213,21 +217,13 @@ const Dashboard = () => {
                             onClick={() => {
                                 window.location = '/upgrade';
                             }}
-                        >
+                        ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                        <path fillRule="evenodd" d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z" clipRule="evenodd" />
+                        </svg>
                             Upgrade
                             {/* Upgrade Icon */}
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                className="w-6 h-6"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M19.5 21a3 3 0 003-3V9a3 3 0 00-3-3h-5.379a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H4.5a3 3 0 00-3 3v12a3 3 0 003 3h15zM9 12.75a.75.75 0 000 1.5h6a.75.75 0 000-1.5H9z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
+                            
+
                         </button>
                         <button
                             className="bg-slate-100/10 hover:bg-slate-100/20 text-white font-bold py-2 px-4 rounded"
@@ -250,6 +246,10 @@ const Dashboard = () => {
             {/* Render user data here */}
             <div className = "pt-16 flex items-center justify-between mx-4">
             <h1 className = "text-2xl font-bold text-gradient bg-gradient-to-r from-purple-300 to-blue-300 text-transparent bg-clip-text">Welcome, {userData.firstName}!</h1>
+            
+            {/* search bar goes here*/}
+            
+            
             <button className = "flex items-center bg-slate-100/10 hover:bg-slate-100/20 text-white font-light py-2 px-3 rounded" onClick={() => handleNewNote()}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path fillRule="evenodd" d="M12 5.25a.75.75 0 01.75.75v5.25H18a.75.75 0 010 1.5h-5.25V18a.75.75 0 01-1.5 0v-5.25H6a.75.75 0 010-1.5h5.25V6a.75.75 0 01.75-.75z" clipRule="evenodd" />
 </svg>
@@ -275,7 +275,13 @@ New</button>
                 </div>
             )    
             }
-
+            <div className = "flex items-center bg-slate-100/10 rounded-md px-2 m-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-slate-100/80">
+                    <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clipRule="evenodd" />
+                    </svg>
+                <input className = "bg-transparent h-auto rounded-md p-1 w-full text-sm focus:outline-none focus:outline-none text-white focus:border-transparent"></input>
+                <button onClick = {(e) => {handleSearch()}} className = "bg-transparent hover:bg-slate-100/20 text-white font-bold py-2 px-2 rounded">Search</button>
+            </div>
 
             {/* THIS SECTION IS FOR FOLDERS, AND IS CURRENTLY UNDER DEVELOPMENT */}
             {folderData.length === 0 ? (
